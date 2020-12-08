bf3de57c68a37c5ec1816ce5eb5ec2da7fdc8053
Louis Petitpez <louispetitpez@gmail.com>
2020-12-08 20:37:45 +0100

    commit

--- app/commands/prefix.js
+++ app/commands/prefix.js
@@ -10,14 +10,23 @@ module.exports = {
             prefix: args[0]
         }
 
-        fs.writeFile("commands/prefixes.json", JSON.stringify(prefix), (err) => {
-            if (err) console.log(err)
-        })
+        // vérifier que arg[0] appartient à une liste de préfixe avant de le modifier
+        // si oui le changer
+        // si non, renvoyer sur le channel un message comme quoi le préfix n'est pas autorisé
+
+        console.log(args[0])
+        if (['%', '!', '#'].includes(args[0])) {
+            fs.writeFile("commands/prefixes.json", JSON.stringify(prefix), (err) => {
+                if (err) console.log(err)
+            })
 
 
-        message.channel.send("new prefix: " + args[0] +  JSON.stringify(prefix))
-        
-        
+            message.channel.send("new prefix: " + args[0] + JSON.stringify(prefix))
+        }
+
+        message.channel.send("le prefixe ne fait pas parti de la liste ['%', '!', '#']")
+
+
     }
 
 };
